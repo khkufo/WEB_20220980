51e44797f08af045caf8a692dfade3e2a043dc15
khkufo <khkufo@naver.com>
2023-03-29 02:10:13 +0000

    0329

---
+++ js/search.js
@@ -0,0 +1,8 @@
+document.getElementByld("search_btn").addEventListener('click', search_message);
+function search_message(){
+	alert("검색을 수행합니다!"); //alert: 메시지 출력
+}
+//getElementbyld 문서의 특정 id를 가져온다.  
+//addEventListner로 이벤트를 등록한다. 
+//click은 마우스 이벤트의 한 종류
+//search_message는 함수이다. 

f6a4eb449910dadb57b94f4a5b2ee04295ec379c
khkufo <khkufo@naver.com>
2023-04-05 02:39:49 +0000

    0405

---
+++ js/basic_test.js
@@ -0,0 +1,21 @@
+var ib='hi'; //변수 선언 후 주석 가능
+//자바스크립트,파이썬 문자는 작은따옴표/큰따옴표 무관
+var a=1;
+var b;
+b=5;
+
+if(true){
+	let c='let 접근'; //지역변수
+	var c_1='var 접근'; //전역변수
+}
+//console.log(c); //if문 내에서만 사용되는 let변수이므로 에러남
+console.log(c_1);
+
+let d=5; //let=var
+//let d='값 재할당'; //let은 값 재할당이 안됨. 
+console.log(d);
+
+const e='상수 1 접근'; //상수는 선언과 동시에 초기화해주기
+//e=5;
+//const f //Error?
+console.log(e);
--- js/search.js
+++ js/search.js
@@ -1,6 +1,9 @@
-document.getElementByld("search_btn").addEventListener('click', search_message);
+document.getElementById("search_btn").addEventListener('click', search_message);
 function search_message(){
 	alert("검색을 수행합니다!"); //alert: 메시지 출력
+	let search_str =document.querySelector("#search_txt"); //변수에 저장
+	document.getElementById("search_message").innerHTML=search_str.value; //태그에 값 추가
+	console.log(search_str.value); //콘솔에 출력
 }
 //getElementbyld 문서의 특정 id를 가져온다.  
 //addEventListner로 이벤트를 등록한다. 

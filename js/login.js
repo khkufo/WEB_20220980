3b26ace37a033373151bfbcfcc2915975857b512
khkufo <khkufo@naver.com>
2023-05-10 02:31:59 +0000

    0510

--- js/login.js
+++ js/login.js
@@ -2,10 +2,19 @@ function login(){
     let form = document.querySelector("#form_main");
     let id = document.querySelector("#floatingInput");
     let password = document.querySelector("#floatingPassword");
+	let check=document.querySelector("#idSaveCheck");
     
     form.action = "../index_login.html";
     form.method = "get";
     
+	if(check.checked==true){//아이디 체크 ㅇ
+		alert("쿠키를 저장합니다");
+		setCookie("id",id.value,1); //1일 저장
+		alert("쿠키 값: "+id.value);
+	}
+	else{//아이디 체크 x 
+		setCookie("id",id.value,0); // 날짜를 0-쿠키 삭제
+	}
     if(id.value.length === 0 || password.value.length === 0){
         alert("아이디와 비밀번호를 모두 입력해주세요.");
     }else{
@@ -35,3 +44,40 @@ function get_id(){
 alert(getParameters('id') + '님 방갑습니다!'); // 메시지 창 출력
 }
 
+function setCookie(name,value,expiredays){
+	var date=new Date(); //현재 시간 기준-date
+	date.setDate(date.getDate()+expiredays); //setDate함수는 시간을 설정
+	document.cookie=escape(name)+"="+escape(value)+";expires="+date.toUTCString()+"SameSite=None;Secure";
+	//getDate 함수는 utc표준 날짜 리턴
+}
+
+function getCookie(name){ //쿠키가 존재하면 얻는다(쿠키는 키,값으로 이루어짐. 값은 인덱스 [1])
+	var cookie=document.cookie;
+	console.log("쿠키를 요청합니다.");
+	if(cookie !=""){
+		var cookie_array=cookie.split(";");
+		for(var index in cookie_array){
+			var cookie_name=cookie_array[index].split("=");
+			if(cookie_name[0]=="id"){ //popupYN을 찾아 값을 리턴
+				return cookie_name[1];
+			}
+		}
+	}
+	return;
+}
+
+function deleteCookie(cookieName){
+	var expireDate=new Date();
+	expireDate.setDate(expireDate.getDate()-1);
+	document.cookie=cookieName+"="+"; expires="+expireDate.toGMTString();
+}
+
+function init(){ //로그인 폼에 쿠키에서 가져온 아이디 입력
+	let id=document.querySelector("#floatingInput");
+	let check=document.querySelector("#idSaveCheck");
+	let get_id=getCookie("id");
+	if(get id){
+		id.value=get_id;
+		check.checked=true;
+	}
+}
--- js/pop_up.js
+++ js/pop_up.js
@@ -1,5 +1,8 @@
 function pop_up(){
-	window.open("../pop_up/pop_up.html","팝업테스트","width=400,height=300,top=10,left=10");
+	var cookieCheck=getCookie("popupYN");
+	if(cookieCheck !="N"){
+		window.open("pop_up.html","팝업테스트","width=400,height=300,top=10,left=10");
+	}
 	//window.open()함수는 창을 열거나 닫는(.close) 전용 함수이다.
 	// ../ 는 상위 경로를 가리키는 것*/
 }
@@ -28,4 +31,34 @@ function show_clock(){
 		divClock.style.color="red";
 	}
 	setTimeout(show_clock, 1000); //1초마다 갱신
+}
+
+function setCookie(name,value,expiredays){
+	var date=new Date(); //현재 시간 기준-date
+	date.setDate(date.getDate()+expiredays); //setDate함수는 시간을 설정
+	document.cookie=escape(name)+"="+escape(value)+";expires="+date.toUTCString()+"SameSite=None;Secure";
+	//getDate 함수는 utc표준 날짜 리턴
+}
+
+function getCookie(name){ //쿠키가 존재하면 얻는다(쿠키는 키,값으로 이루어짐. 값은 인덱스 [1])
+	var cookie=document.cookie;
+	console.log("쿠키를 요청합니다.");
+	if(cookie !=""){
+		var cookie_array=cookie.split(";");
+		for(var index in cookie_array){
+			var cookie_name=cookie_array[index].split("=");
+			if(cookie_name[0]=="popupYN"){ //popupYN을 찾아 값을 리턴
+				return cookie_name[1];
+			}
+		}
+	}
+	return;
+}
+
+function closePopup(){ //체크박스 클릭시 윈ㄷ우 닫는 함수-id값이 존재하면 클릭후 쿠키를 set
+	if(document.getElementById('check_popup').value){
+		setCookie("popupYN","N",1);
+		console.log("쿠키를 설정합니다.");
+		self.close(); //현재 보고 있는 창 자신을 닫는다.
+	}
 }
